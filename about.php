08f0dbba17cc5d2597c2d020e0ddb8bc441c921e
kevinZaffino <alpha00017@gmail.com>
2016-06-09 13:30:29 +0200

    Documentation

--- about.php
+++ about.php
@@ -24,7 +24,7 @@ and open the template in the editor.
                 <h3 class = "panel-title">A propos du site</h3>
             </div>
             <div class="panel-body">
-                xgxcgxf
+                <p>Le site permettra aux utilisateurs de poster diverses annonces (ventes, services, …). Les utilisateurs pourront voir les annonces existantes et auront ainsi la possibilité de contacter la personne concernée.</p>
             </div>
         </div>
 
@@ -33,7 +33,26 @@ and open the template in the editor.
                 <h3 class = "panel-title">F.A.Q</h3>
             </div>
             <div class="panel-body">
-                gdfgdxgdgdxygdfgx
+                <h2>
+                    Comment se connecter sur le site ?
+                </h2>
+                <p>
+                    Il suffit de cliquer sur l’onglet « Connexion/Inscription » dans le menu du site.
+                </p>
+                <h2>
+                </h2>
+                <h2>
+                    Comment ajouter une nouvelle annonce ?
+                </h2>
+                <p>
+                    Une fois connecté sur le site, il suffit de cliquer sur « Nouvelle annonce » dans la page d’accueil.
+                </p>
+                <h2>
+                    Je possède une annonce qui est terminée, comment le signaler ?
+                </h2>
+                <p>
+                    Il suffit de cliquer sur « Ouvrir/Fermer » dans la page de votre annonce.
+                </p>
             </div>
         </div>
 
